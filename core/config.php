5010c19f838e07e6ce5c5fe50364406ec0c48d73
Abae Akili <mohaabae@yahoo.com>
2020-11-16 11:00:24 +0300

    📤 Added Config for database and smtp connection

---
+++ core/config.php
@@ -0,0 +1,15 @@
+<?php 
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'login');
+	define('DB_USER', 'login');
+	define('DB_PASS', '');
+	define('BASE_URL', 'http://http://localhost/sms-login-verification-system-php-mysql/');
+
+	//SMTP
+	define("M_HOST", 'smtp.gmail.com');
+	define("M_USERNAME", 'email@gmail.com');
+	define("M_PASSWORD", 'emailPassword');
+	define("M_SMTPSECURE", 'ssl');
+	define("M_PORT", "465");
+	
+?>
